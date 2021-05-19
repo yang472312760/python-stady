c4a7579661359e5e302d1808ce4735c8e77377d1
yangchenglin <yangchenglin5533@163.com>
2021-05-19 11:24:35 +0800

    变量

---
+++ IncrementalAssignment.py
@@ -0,0 +1,14 @@
+# 增量赋值
+m = 12
+
+m %= 7
+
+print(m)
+m **= 2
+
+print(m)
+
+aList = [123, 'xyz']
+aList += [45.6e7]
+
+print(aList)
---
+++ MultipleAssignment.py
@@ -0,0 +1,18 @@
+'''
+多重赋值
+'''
+
+x = y = z = 1
+
+print(x)
+print(y)
+print(z)
+
+'''
+多元赋值
+'''
+x1, y1, z1 = 1, 2, 'a string'
+
+print(x1)
+print(y1)
+print(z1)
